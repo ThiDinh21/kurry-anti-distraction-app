c1df4d165f609233f234e4e4ebbdfdbc0f5285fc
VietNguyen408 <76224694+VietNguyen408@users.noreply.github.com>
2021-03-07 16:47:42 +0200

    Finish Character Shop

---
+++ UI/Shop/ShopPanel/PurchaseButton.gd
@@ -0,0 +1,19 @@
+extends Button
+
+onready var currency = Global.game_state["currency"]
+onready var price = get_parent().get_node("PriceLabel").price
+	
+func _process(_delta):
+	self.disabled = true if (currency < price) else false
+	
+func _buy(price):
+	if Global.game_state["currency"] >= price:
+		Global.game_state["currency"] -= price
+		Global.save_game()
+	else:
+		var rem = price - Global.game_state["currency"]
+		print(rem)
+
+
+func _on_PurchaseButton_pressed():
+	_buy(price)
