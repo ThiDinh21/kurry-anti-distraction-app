2acc31464db6309f687fcdaa68418fa947ae3b1c
VietNguyen408 <76224694+VietNguyen408@users.noreply.github.com>
2021-03-24 15:05:30 +0200

    Add leaderboard

--- UI/Home/Home.gd
+++ UI/Home/Home.gd
@@ -2,3 +2,36 @@ extends Control
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://UI/Menu.tscn")
+
+
+func _on_1st_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+	
+
+
+func _on_2nd_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+
+
+func _on_3rd_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+	
+
+func _on_4th_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+
+
+func _on_5th_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+
+
+func _on_6th_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+
+
+func _on_7th_Floor_pressed():
+	get_tree().change_scene("res://TestFloor.tscn")
+
+
+func _on_Last_Floor_pressed():
+	pass # Focus mode scene
